229e0fc035f837efee206c16cea7d6cc4b961816
Anton Garbov <anton.garbov@gmail.com>
2018-06-26 23:28:02 +0300

    Initial commit

---
+++ app/src/main/java/com/antondevs/apps/githubbrowser/data/entries/RepoEntry.java
@@ -0,0 +1,84 @@
+package com.antondevs.apps.githubbrowser.data.entries;
+
+/**
+ * Created by Anton on 6/24/18.
+ */
+
+public class RepoEntry {
+
+    private String mOwnerName;
+    private String mRepoName;
+
+    private String mContributorsUrl;
+    private String mOwnerAvatarUrl;
+    private String mBranchesUrl;
+
+    private String mAvatarStoragePath;
+
+    private int mForksCount;
+    private int mStarCount;
+
+    public String getmOwnerName() {
+        return mOwnerName;
+    }
+
+    public void setmOwnerName(String mOwnerName) {
+        this.mOwnerName = mOwnerName;
+    }
+
+    public String getmRepoName() {
+        return mRepoName;
+    }
+
+    public void setmRepoName(String mRepoName) {
+        this.mRepoName = mRepoName;
+    }
+
+    public String getmContributorsUrl() {
+        return mContributorsUrl;
+    }
+
+    public void setmContributorsUrl(String mContributorsUrl) {
+        this.mContributorsUrl = mContributorsUrl;
+    }
+
+    public String getmOwnerAvatarUrl() {
+        return mOwnerAvatarUrl;
+    }
+
+    public void setmOwnerAvatarUrl(String mOwnerAvatarUrl) {
+        this.mOwnerAvatarUrl = mOwnerAvatarUrl;
+    }
+
+    public String getmBranchesUrl() {
+        return mBranchesUrl;
+    }
+
+    public void setmBranchesUrl(String mBranchesUrl) {
+        this.mBranchesUrl = mBranchesUrl;
+    }
+
+    public String getmAvatarStoragePath() {
+        return mAvatarStoragePath;
+    }
+
+    public void setmAvatarStoragePath(String mAvatarStoragePath) {
+        this.mAvatarStoragePath = mAvatarStoragePath;
+    }
+
+    public int getmForksCount() {
+        return mForksCount;
+    }
+
+    public void setmForksCount(int mForksCount) {
+        this.mForksCount = mForksCount;
+    }
+
+    public int getmStarCount() {
+        return mStarCount;
+    }
+
+    public void setmStarCount(int mStarCount) {
+        this.mStarCount = mStarCount;
+    }
+}
---
+++ app/src/main/java/com/antondevs/apps/githubbrowser/data/entries/UserEntry.java
@@ -0,0 +1,74 @@
+package com.antondevs.apps.githubbrowser.data.entries;
+
+/**
+ * Created by Anton on 6/24/18.
+ */
+public class UserEntry {
+
+    private String mLoginName;
+
+    private String mAvatarUrl;
+    private String mFollowersUrl;
+    private String mFollowingUrl;
+
+    private String mAvatarStoragePath;
+
+    private String mStarredUrl;
+    private String mReposUrl;
+
+    public String getmLoginName() {
+        return mLoginName;
+    }
+
+    public void setmLoginName(String mLoginName) {
+        this.mLoginName = mLoginName;
+    }
+
+    public String getmAvatarUrl() {
+        return mAvatarUrl;
+    }
+
+    public void setmAvatarUrl(String mAvatarUrl) {
+        this.mAvatarUrl = mAvatarUrl;
+    }
+
+    public String getmFollowersUrl() {
+        return mFollowersUrl;
+    }
+
+    public void setmFollowersUrl(String mFollowersUrl) {
+        this.mFollowersUrl = mFollowersUrl;
+    }
+
+    public String getmFollowingUrl() {
+        return mFollowingUrl;
+    }
+
+    public void setmFollowingUrl(String mFollowingUrl) {
+        this.mFollowingUrl = mFollowingUrl;
+    }
+
+    public String getmAvatarStoragePath() {
+        return mAvatarStoragePath;
+    }
+
+    public void setmAvatarStoragePath(String mAvatarStoragePath) {
+        this.mAvatarStoragePath = mAvatarStoragePath;
+    }
+
+    public String getmStarredUrl() {
+        return mStarredUrl;
+    }
+
+    public void setmStarredUrl(String mStarredUrl) {
+        this.mStarredUrl = mStarredUrl;
+    }
+
+    public String getmReposUrl() {
+        return mReposUrl;
+    }
+
+    public void setmReposUrl(String mReposUrl) {
+        this.mReposUrl = mReposUrl;
+    }
+}
