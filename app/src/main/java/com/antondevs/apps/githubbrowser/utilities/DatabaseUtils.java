76c6db4b3ea7830055f3040a7fc0d613a91e4182
Anton Garbov <anton.garbov@gmail.com>
2018-06-29 00:58:26 +0300

    Define basic testing utils class.

---
+++ app/src/main/java/com/antondevs/apps/githubbrowser/utilities/DatabaseUtils.java
@@ -0,0 +1,23 @@
+package com.antondevs.apps.githubbrowser.utilities;
+
+import com.antondevs.apps.githubbrowser.data.entries.UserEntry;
+
+import java.util.List;
+
+/**
+ * Created by Anton on 6/29/18.
+ */
+public final class DatabaseUtils {
+
+    private static String[] names = {"mini", "roger", "dans", "helicopter", "restless", "george"};
+
+    private static int[] integer_values = {23, 67, 2, 18, 45, 33, 29, 56, 100, 174, 99, 39};
+
+    public static UserEntry generateUser() {
+        return null;
+    }
+
+    public static List<UserEntry> createEntriesList() {
+        return null;
+    }
+}
