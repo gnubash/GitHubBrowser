1964b9b0f5691e8ea7778c601a4897ce0b811e10
Anton Garbov <anton.garbov@gmail.com>
2018-06-28 22:22:41 +0300

    Add new members and getters/setters.

--- app/src/main/java/com/antondevs/apps/githubbrowser/data/entries/UserEntry.java
+++ app/src/main/java/com/antondevs/apps/githubbrowser/data/entries/UserEntry.java
@@ -1,5 +1,7 @@
 package com.antondevs.apps.githubbrowser.data.entries;
 
+import java.util.List;
+
 /**
  * Created by Anton on 6/24/18.
  */
@@ -9,9 +11,22 @@ public class UserEntry {
 
     private String mLoginName;
 
+    private int mFollowers;
+    private int mFollowing;
+
+    private List<String> mOwnedRepos;
+    private List<String> mStarredRepos;
 
     public UserEntry(){
 
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
     }
 
     public String getmLoginName() {
@@ -21,6 +36,36 @@ public class UserEntry {
     public void setmLoginName(String mLoginName) {
         this.mLoginName = mLoginName;
     }
-    
 
+    public int getmFollowers() {
+        return mFollowers;
+    }
+
+    public void setmFollowers(int mFollowers) {
+        this.mFollowers = mFollowers;
+    }
+
+    public int getmFollowing() {
+        return mFollowing;
+    }
+
+    public void setmFollowing(int mFollowing) {
+        this.mFollowing = mFollowing;
+    }
+
+    public List<String> getmOwnedRepos() {
+        return mOwnedRepos;
+    }
+
+    public void setmOwnedRepos(List<String> mOwnedRepos) {
+        this.mOwnedRepos = mOwnedRepos;
+    }
+
+    public List<String> getmStarredRepos() {
+        return mStarredRepos;
+    }
+
+    public void setmStarredRepos(List<String> mStarredRepos) {
+        this.mStarredRepos = mStarredRepos;
+    }
 }
