e95aa5cc4d35e320a96275e018eaa5bbbfcb8b0f
Anton Garbov <anton.garbov@gmail.com>
2018-06-30 14:56:33 +0300

    Add TextView for testing and reference it.

--- app/src/main/java/com/antondevs/apps/githubbrowser/LoginActivity.java
+++ app/src/main/java/com/antondevs/apps/githubbrowser/LoginActivity.java
@@ -2,6 +2,7 @@ package com.antondevs.apps.githubbrowser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.antondevs.apps.githubbrowser.data.GitHubBrowserDatabase;
 import com.antondevs.apps.githubbrowser.data.entries.UserEntry;
@@ -15,16 +16,20 @@ public class LoginActivity extends AppCompatActivity {
 
     private GitHubBrowserDatabase mAppDatabase;
 
+    private TextView mLoginActivityTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mLoginActivityTextView = (TextView) findViewById(R.id.tv_login_activity);
+
         mAppDatabase = GitHubBrowserDatabase.getDatabaseInstance(getApplicationContext());
 
         writeDataToDatabase();
 
-        
+
     }
 
 
