7edb9b2211ff82078c73be8dc9d2f7aaf0e86189
Anton Garbov <anton.garbov@gmail.com>
2018-06-30 13:50:25 +0300

    Add two utils methods for creating UserEntry and RepoEntry.

--- app/src/main/java/com/antondevs/apps/githubbrowser/utilities/DatabaseUtils.java
+++ app/src/main/java/com/antondevs/apps/githubbrowser/utilities/DatabaseUtils.java
@@ -1,8 +1,10 @@
 package com.antondevs.apps.githubbrowser.utilities;
 
+import com.antondevs.apps.githubbrowser.data.entries.RepoEntry;
 import com.antondevs.apps.githubbrowser.data.entries.UserEntry;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Anton on 6/29/18.
@@ -14,7 +16,24 @@ public final class DatabaseUtils {
     private static int[] integer_values = {23, 67, 2, 18, 45, 33, 29, 56, 100, 174, 99, 39};
 
     public static UserEntry generateUser() {
-        return null;
+        UserEntry user = new UserEntry();
+        Random random = new Random();
+
+        user.setLogin(names[random.nextInt(names.length)]);
+        user.setFollowers(integer_values[random.nextInt(integer_values.length)]);
+        user.setFollowing(integer_values[random.nextInt(integer_values.length)]);
+        return user;
+    }
+
+    public static RepoEntry generateRepor() {
+        RepoEntry repo = new RepoEntry();
+        Random random = new Random();
+
+        repo.setLogin(names[random.nextInt(names.length)]);
+        repo.setName(names[random.nextInt(names.length)]);
+        repo.setForks(integer_values[random.nextInt(integer_values.length)]);
+        repo.setWatchers(integer_values[random.nextInt(integer_values.length)]);
+        return repo;
     }
 
     public static List<UserEntry> createEntriesList() {
