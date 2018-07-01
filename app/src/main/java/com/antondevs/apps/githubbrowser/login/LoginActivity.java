2176c1fd069247687fa245c400ceb526d2e4abe3
Anton Garbov <anton.garbov@gmail.com>
2018-07-01 12:49:59 +0300

    Implement LoginView in LoginActivity.

--- app/src/main/java/com/antondevs/apps/githubbrowser/login/LoginActivity.java
+++ app/src/main/java/com/antondevs/apps/githubbrowser/login/LoginActivity.java
@@ -2,21 +2,39 @@ package com.antondevs.apps.githubbrowser.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.antondevs.apps.githubbrowser.R;
+import com.antondevs.apps.githubbrowser.data.TestInteractorImp;
 
-public class LoginActivity extends AppCompatActivity implements LoginContract.LoginView{
+public class LoginActivity extends AppCompatActivity implements LoginContract.LoginView {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+    private TextView errorMessage;
+
+    private LoginContract.LoginPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        usernameEditText = findViewById(R.id.login_username_edit_text);
+        passwordEditText = findViewById(R.id.login_password_edit_text);
+        loginButton = findViewById(R.id.login_button_login);
+        errorMessage = findViewById(R.id.login_error_message_text_view);
+
+        presenter = new LoginPresenterImp(this, new TestInteractorImp());
     }
 
     @Override
     public void requestAuthentication() {
-
+        displayLoginViews();
     }
 
     @Override
@@ -26,6 +44,12 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Lo
 
     @Override
     public void displayErrorMessage() {
+        errorMessage.setVisibility(View.VISIBLE);
+    }
 
+    private void displayLoginViews() {
+        usernameEditText.setVisibility(View.VISIBLE);
+        passwordEditText.setVisibility(View.VISIBLE);
+        loginButton.setVisibility(View.VISIBLE);
     }
 }
