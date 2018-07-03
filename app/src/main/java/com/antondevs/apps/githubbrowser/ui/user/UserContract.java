4cfa26d77ac214a19aacdbc3e4860eaec6191da4
Anton Garbov <anton.garbov@gmail.com>
2018-07-03 20:27:53 +0300

    View diff to check change. Also add two packages for the rest of the ui.

--- app/src/main/java/com/antondevs/apps/githubbrowser/ui/user/UserContract.java
+++ app/src/main/java/com/antondevs/apps/githubbrowser/ui/user/UserContract.java
@@ -1,5 +1,9 @@
 package com.antondevs.apps.githubbrowser.ui.user;
 
+import com.antondevs.apps.githubbrowser.data.database.UserEntry;
+
+import java.util.List;
+
 /**
  * Created by Anton on 7/1/18.
  */
@@ -12,15 +16,18 @@ public interface UserContract {
         void setFollowers(String followersNumber);
 
         void setFollowing(String followingNumber);
+
+        void setFollowList(List<UserEntry> userEntryList);
     }
 
 
     interface UserPresenter {
 
-        void init();
+        void loadUser(String username);
 
         void getFollowers(String username);
 
         void getFollowing(String username);
+
     }
 }
