63d917b4148bae08f622664d795874e57b6c13a5
Anton Garbov <anton.garbov@gmail.com>
2018-07-03 21:20:28 +0300

    Add remaining contracts for repo and search screens. Methods not defined yet.

---
+++ app/src/main/java/com/antondevs/apps/githubbrowser/ui/repo/RepoContract.java
@@ -0,0 +1,15 @@
+package com.antondevs.apps.githubbrowser.ui.repo;
+
+/**
+ * Created by Anton on 7/3/18.
+ */
+public interface RepoContract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+}
---
+++ app/src/main/java/com/antondevs/apps/githubbrowser/ui/search/SearchContract.java
@@ -0,0 +1,15 @@
+package com.antondevs.apps.githubbrowser.ui.search;
+
+/**
+ * Created by Anton on 7/3/18.
+ */
+public interface SearchContract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+}
