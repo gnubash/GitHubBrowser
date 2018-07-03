16120d9872d971c52c09fe95ea40666bba128c6b
Anton Garbov <anton.garbov@gmail.com>
2018-07-03 22:28:33 +0300

    Define SearchContract.

--- app/src/main/java/com/antondevs/apps/githubbrowser/ui/search/SearchContract.java
+++ app/src/main/java/com/antondevs/apps/githubbrowser/ui/search/SearchContract.java
@@ -1,5 +1,9 @@
 package com.antondevs.apps.githubbrowser.ui.search;
 
+import com.antondevs.apps.githubbrowser.data.database.UserEntry;
+
+import java.util.List;
+
 /**
  * Created by Anton on 7/3/18.
  */
@@ -7,9 +11,15 @@ public interface SearchContract {
 
     interface View {
 
+        void setSearchResult(List<UserEntry> userList);
+
+        void showNoResultsView();
+
     }
 
     interface Presenter {
 
+        void searchUser(String username);
+        
     }
 }
