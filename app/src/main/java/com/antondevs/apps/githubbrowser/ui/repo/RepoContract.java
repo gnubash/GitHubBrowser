731213efdefe2317aa334bb375172a57f1d13a20
Anton Garbov <anton.garbov@gmail.com>
2018-07-03 21:59:58 +0300

    Define RepoContract interfaces.

--- app/src/main/java/com/antondevs/apps/githubbrowser/ui/repo/RepoContract.java
+++ app/src/main/java/com/antondevs/apps/githubbrowser/ui/repo/RepoContract.java
@@ -1,5 +1,9 @@
 package com.antondevs.apps.githubbrowser.ui.repo;
 
+import com.antondevs.apps.githubbrowser.data.database.UserEntry;
+
+import java.util.List;
+
 /**
  * Created by Anton on 7/3/18.
  */
@@ -7,9 +11,23 @@ public interface RepoContract {
 
     interface View {
 
+        void setOwner(String ownerName);
+
+        void setCommits(String commitsNumber);
+
+        void setBranches(String branchesNumber);
+
+        void setContributors(String contributorsNumber);
+
+        void setStar(String starNumber);
+
+        void setFork(String forkNumber);
+
+        void setRepoName(String repoName);
     }
 
     interface Presenter {
 
+        void loadRepository(String name);
     }
 }
