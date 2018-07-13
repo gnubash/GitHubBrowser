a5bedf9230c5f19167f855b1ccf121c0384ea5ac
Anton Garbov <anton.garbov@gmail.com>
2018-07-14 00:46:41 +0300

    Enable DataBinding for porject. Use DataBinding to aquire all TextViews in RepoActivity.

--- app/src/main/java/com/antondevs/apps/githubbrowser/ui/repo/RepoActivity.java
+++ app/src/main/java/com/antondevs/apps/githubbrowser/ui/repo/RepoActivity.java
@@ -1,15 +1,23 @@
 package com.antondevs.apps.githubbrowser.ui.repo;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.antondevs.apps.githubbrowser.R;
+import com.antondevs.apps.githubbrowser.databinding.ActivityRepoBinding;
 
 public class RepoActivity extends AppCompatActivity {
 
+    private static final String LOGTAG = RepoActivity.class.getSimpleName();
+
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repo);
+
+        ActivityRepoBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_repo);
     }
 }
