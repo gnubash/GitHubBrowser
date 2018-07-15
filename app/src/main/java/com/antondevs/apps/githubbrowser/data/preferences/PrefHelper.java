91390e65d98376eaae7a220d0e5969004a069375
Anton Garbov <anton.garbov@gmail.com>
2018-07-15 23:54:29 +0300

    Add PreferenceHelper interface and implementation. SharedPreference object should be passed to the helper when an instance is requested.

---
+++ app/src/main/java/com/antondevs/apps/githubbrowser/data/preferences/PrefHelper.java
@@ -0,0 +1,13 @@
+package com.antondevs.apps.githubbrowser.data.preferences;
+
+/**
+ * Created by Anton.
+ */
+public interface PrefHelper {
+
+    void addUserCredentials(String username, String password);
+
+    boolean isAuthenticated();
+
+    void userAuthenticated(boolean status);
+}
