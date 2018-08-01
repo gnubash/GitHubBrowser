8a50e7f9785cee943f4ed83f2e5bcf5d755e0110
Anton Garbov <anton.garbov@gmail.com>
2018-08-02 00:41:39 +0300

    Add onConflictStrategy to insert. Change query for getAuth to always return first row.

--- app/src/main/java/com/antondevs/apps/githubbrowser/data/database/AuthDao.java
+++ app/src/main/java/com/antondevs/apps/githubbrowser/data/database/AuthDao.java
@@ -3,6 +3,7 @@ package com.antondevs.apps.githubbrowser.data.database;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 
 import java.util.List;
@@ -13,13 +14,13 @@ import java.util.List;
 @Dao
 public interface AuthDao {
 
-    @Query("SELECT * FROM auth")
+    @Query("SELECT * FROM auth WHERE id = 0")
     AuthEntry getAuth();
 
     @Query("SELECT Count(*) FROM auth")
     int getNumberOfStoredCredentials();
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertAuth(AuthEntry entry);
 
     @Query("DELETE FROM auth")
