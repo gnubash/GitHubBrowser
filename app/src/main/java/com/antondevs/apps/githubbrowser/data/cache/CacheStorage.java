c41f27bbf24cfff506338510b907cc00b154b029
Anton Garbov <anton.garbov@gmail.com>
2018-11-04 09:19:37 +0200

    Replace temp Map-based cache with Guava Cache

--- app/src/main/java/com/antondevs/apps/githubbrowser/data/cache/CacheStorage.java
+++ app/src/main/java/com/antondevs/apps/githubbrowser/data/cache/CacheStorage.java
@@ -4,14 +4,12 @@ import android.util.Log;
 
 import com.antondevs.apps.githubbrowser.data.database.model.RepoEntry;
 import com.antondevs.apps.githubbrowser.data.database.model.UserEntry;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Maybe;
-import io.reactivex.Observable;
-import io.reactivex.Single;
 
 /**
  * Created by Anton.
@@ -20,61 +18,85 @@ public class CacheStorage implements LocalCache{
 
     private static final String LOGTAG = CacheStorage.class.getSimpleName();
 
+    private Cache<String, UserEntry> userCache;
+    private Cache<String, RepoEntry> repoCache;
+
+    private long cacheCapacity = 500L;
+    private long expireTime = 10L;
+    private TimeUnit expireTimeUnit = TimeUnit.MINUTES;
+
     private long observableDelay = 100L;
     private TimeUnit timeUnitMillis = TimeUnit.MILLISECONDS;
 
-    private Map<String, UserEntry> userCache;
-    private Map<String, RepoEntry> repoCache;
-
     private CacheStorage() {
         Log.d(LOGTAG, "CacheStorage");
-        userCache = new HashMap<>();
-        repoCache = new HashMap<>();
+        createCaches();
     }
 
     public static LocalCache getInstance() {
-        Log.d(LOGTAG, "CacheStorage.getInstance");
+        Log.d(LOGTAG, "getInstance");
         return new CacheStorage();
     }
 
     @Override
     public Maybe<UserEntry> getUser(String loginName) {
-        Log.d(LOGTAG, "CacheStorage.getUser");
-        return (userCache.containsKey(loginName))
-                ? Maybe.just(userCache.get(loginName)).delay(observableDelay, timeUnitMillis) : Maybe.<UserEntry>empty();
+        Log.d(LOGTAG, "getUser");
+        UserEntry tempUser = userCache.getIfPresent(loginName);
+
+        return (tempUser == null) ? Maybe.<UserEntry>empty() : Maybe.just(tempUser).delay(observableDelay, timeUnitMillis);
+
     }
 
     @Override
     public Maybe<RepoEntry> getRepo(String repoFullName) {
-        Log.d(LOGTAG, "CacheStorage.getRepo");
-        return (repoCache.containsKey(repoFullName))
-                ? Maybe.just(repoCache.get(repoFullName)).delay(observableDelay, timeUnitMillis) : Maybe.<RepoEntry>empty();
+        Log.d(LOGTAG, "getRepo");
+        RepoEntry tempRepo = repoCache.getIfPresent(repoFullName);
+        return (tempRepo == null) ? Maybe.<RepoEntry>empty() : Maybe.just(tempRepo).delay(observableDelay, timeUnitMillis);
+
     }
 
     @Override
     public void addUser(UserEntry userEntry) {
-        Log.d(LOGTAG, "CacheStorage.addUser");
+        Log.d(LOGTAG, "addUser");
         userCache.put(userEntry.getLogin(), userEntry);
     }
 
     @Override
     public void addRepo(RepoEntry repoEntry) {
-        Log.d(LOGTAG, "CacheStorage.addRepo");
+        Log.d(LOGTAG, "addRepo");
         repoCache.put(repoEntry.getFull_name(), repoEntry);
     }
 
     @Override
     public String getRepoContributorsUrl(String repoName) {
-        return repoCache.get(repoName).getContributors_url();
+        RepoEntry tempRepo = repoCache.getIfPresent(repoName);
+        Log.d(LOGTAG, "getRepoContributorsUrl tempRepo " + tempRepo);
+        return (tempRepo == null) ? "" : tempRepo.getContributors_url();
     }
 
     @Override
     public String getUserFollowersUrl(String loginName) {
-        return userCache.get(loginName).getFollowers_url();
+        UserEntry tempUser = userCache.getIfPresent(loginName);
+        Log.d(LOGTAG, "getUserFollowersUrl tempUser " + tempUser);
+        return (tempUser == null) ? "" : tempUser.getFollowers_url();
     }
 
     @Override
     public String getUserFollowingUrl(String loginName) {
-        return userCache.get(loginName).getFollowing_url();
+        UserEntry tempUser = userCache.getIfPresent(loginName);
+        Log.d(LOGTAG, "getUserFollowingUrl tempUser " + tempUser);
+        return (tempUser == null) ? "" : tempUser.getFollowing_url();
+    }
+
+    private void createCaches() {
+        userCache = CacheBuilder.newBuilder()
+                .maximumSize(cacheCapacity)
+                .expireAfterWrite(expireTime, expireTimeUnit)
+                .build();
+
+        repoCache = CacheBuilder.newBuilder()
+                .maximumSize(cacheCapacity)
+                .expireAfterWrite(expireTime, expireTimeUnit)
+                .build();
     }
 }
